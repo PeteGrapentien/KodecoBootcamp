ae88f7f1d4bcb564529292c50f9e6fcb24cc04f5
Peter Grapentien <a100933@qh623x3nf2.prog1.com>
2024-10-20 15:27:19 -0400

    Adjusted framework for clarity

--- 4-third-party-models/item-identifier/item-identifier/ContentView.swift
+++ 4-third-party-models/item-identifier/item-identifier/ContentView.swift
@@ -9,61 +9,7 @@ struct ContentView: View {
   @State private var detectedObjects: [DetectedObject] = []
 
   func runModel() {
-    guard
-      // 1
-      let cgImage = cgImage,
-      // 2
-      // NOTE: you will need to import the model files in Lesson 3, Instruction 1 for this project to compile. We aren't included
-      let model = try? yolov8x_oiv7(configuration: .init()).model,
-      // 3
-      let detector = try? VNCoreMLModel(for: model) else {
-        // 4
-        print("Unable to load photo.")
-        return
-    }
-    // 1
-    let visionRequest = VNCoreMLRequest(model: detector) { request, error in
-      detectedObjects = []
-      if let error = error {
-        print(error.localizedDescription)
-        return
-      }
-      // 2
-      if let results = request.results as? [VNRecognizedObjectObservation] {
-        // 1
-        if results.isEmpty {
-          print("No results found.")
-          return
-        }
-        // 2
-        for result in results {
-          // 3
-          if let firstIdentifier = result.labels.first {
-            let confidence = firstIdentifier.confidence
-            let label = firstIdentifier.identifier
-            // 4
-            let boundingBox = result.boundingBox
-            // 5
-            let object = DetectedObject(
-              label: label,
-              confidence: confidence,
-              boundingBox: boundingBox
-            )
-            detectedObjects.append(object)
-          }
-        }
-      }
-    }
-    // 1
-    visionRequest.imageCropAndScaleOption = .scaleFill
-    // 2
-    let handler = VNImageRequestHandler(cgImage: cgImage, orientation: .up)
-    // 3
-    do {
-      try handler.perform([visionRequest])
-    } catch {
-      print(error)
-    }
+      
   }
   
   var body: some View {
