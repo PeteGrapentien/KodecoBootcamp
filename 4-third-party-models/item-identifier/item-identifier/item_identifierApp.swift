ee64b7f24aca4c8242012dc362ca7f966b655202
Peter Grapentien <a100933@qh623x3nf2.prog1.com>
2024-10-20 12:36:55 -0400

    Added empty base project

---
+++ 4-third-party-models/item-identifier/item-identifier/item_identifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  item_identifierApp.swift
+//  item-identifier
+//
+//  Created by Peter Grapentien on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct item_identifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
